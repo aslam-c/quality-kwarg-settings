52e87b04cf94ac5e48c42f1c09eea1ab1190c2d2
aslam <47080903+aslam-c@users.noreply.github.com>
2022-05-05 23:13:41 +0530

    Add files via upload

---
+++ admin.bat
@@ -0,0 +1,3 @@
+start chrome http://127.0.0.1:8001
+dynamodb-admin
+
---
+++ start_env.bat
@@ -0,0 +1,3 @@
+cd /
+cd Users\web\Desktop\rbbq\dynamodb_local_latest
+java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
